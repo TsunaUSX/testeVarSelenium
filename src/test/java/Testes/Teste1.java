b9dcb0fa335e8c5c2060bc6837f48c0137ef472d
galplis190@gmail.com <2903199ga>
2021-07-05 09:41:02 -0300

    add boolean param

--- src/test/java/Testes/Teste1.java
+++ src/test/java/Testes/Teste1.java
@@ -11,5 +11,7 @@ public class Teste1 {
 	{
 		System.out.println("Recebi o parâmetro: " + System.getProperty("parametro1"));
 		Assert.assertTrue(System.getProperty("parametro1").equals("123454321"));
+		System.out.println("Valor boolean: " + Boolean.getBoolean("paramBool"));
+		Assert.assertTrue(Boolean.getBoolean("paramBool"));
 	}
 }
