67a0e4ae2ba74ba804bdb5a6ad97ad3381fe26b2
galplis190@gmail.com <2903199ga>
2022-01-08 17:11:23 -0300

    teste API

--- src/test/java/Testes/Teste1.java
+++ src/test/java/Testes/Teste1.java
@@ -1,7 +1,12 @@
 package Testes;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.testng.annotations.Test;
+
+import files.payload;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Teste1 {
 	
@@ -13,5 +18,12 @@ public class Teste1 {
 		Assert.assertTrue(System.getProperty("parametro1").equals("123454321"));
 		System.out.println("Valor boolean: " + Boolean.getBoolean("paramBool"));
 		Assert.assertTrue(Boolean.getBoolean("paramBool"));
+		
+		
+		RestAssured.baseURI= "https://rahulshettyacademy.com";
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(payload.AddPlace()).when().post("maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+		.header("server", "Apache/2.4.18 (Ubuntu)").extract().response();
 	}
 }
